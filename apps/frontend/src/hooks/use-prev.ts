577d4bae2aaa130cce72a104ddef65f35adf3ced
Stan de La Comble <s.delacomble@gmail.com>
2026-02-13 20:58:22 +0100

    Remove use-prev JSdoc comment

--- apps/frontend/src/hooks/use-prev.ts
+++ apps/frontend/src/hooks/use-prev.ts
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react';
 
-/** Returns a ref that points to the previous value of the given value. */
 export const usePrevRef = <T>(value: T) => {
 	const prevRef = useRef<T | undefined>(undefined);
 
