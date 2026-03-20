c3b13cfc04b0bb1101a28775ca5dd500e7df422e
Christophe Blefari <christophe.blefari@gmail.com>
2026-03-20 19:08:34 +0100

    Fix migrations

--- apps/backend/migrations-sqlite/0028_live_stories.sql
+++ apps/backend/migrations-sqlite/0028_live_stories.sql
@@ -1,6 +1,3 @@
-ALTER TABLE `story_version` ADD `is_live` integer DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE `story_version` ADD `cache_schedule` text;--> statement-breakpoint
-ALTER TABLE `story_version` ADD `refresh_text` integer DEFAULT false NOT NULL;--> statement-breakpoint
 CREATE TABLE `story_data_cache` (
 	`chat_id` text NOT NULL,
 	`story_id` text NOT NULL,
@@ -10,3 +7,7 @@ CREATE TABLE `story_data_cache` (
 	PRIMARY KEY(`chat_id`, `story_id`),
 	FOREIGN KEY (`chat_id`) REFERENCES `chat`(`id`) ON UPDATE no action ON DELETE cascade
 );
+--> statement-breakpoint
+ALTER TABLE `story_version` ADD `is_live` integer DEFAULT false NOT NULL;--> statement-breakpoint
+ALTER TABLE `story_version` ADD `cache_schedule` text;--> statement-breakpoint
+ALTER TABLE `story_version` ADD `refresh_text` integer DEFAULT false NOT NULL;
